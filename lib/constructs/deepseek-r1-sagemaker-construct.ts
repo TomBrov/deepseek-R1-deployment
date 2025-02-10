08398ccbf83f2bdd398959673b984db417efaba3
TomBrov <tbrovy@gmail.com>
2025-02-10 18:41:47 +0200

    change construct structure

--- lib/constructs/deepseek-r1-sagemaker-construct.ts
+++ lib/constructs/deepseek-r1-sagemaker-construct.ts
@@ -7,6 +7,9 @@ export interface DeepSeekR1SageMakerProps {
     containerType: string;
     instanceType: string;
     modelName: string;
+    sagemakerRole: SageMakerIamRoleConstruct;
+    inferenceImageUri: string;
+    numberOfGpu: number;
 }
 
 export class DeepSeekR1SageMakerConstruct extends Construct {
@@ -15,39 +18,14 @@ export class DeepSeekR1SageMakerConstruct extends Construct {
     constructor(scope: Construct, id: string, props: DeepSeekR1SageMakerProps) {
         super(scope, id);
 
-        const ecrRepository = props.containerType === "tgi"
-            ? "huggingface-pytorch-tgi-inference"
-            : "djl-inference";
-
-        const inferenceImageUri = props.containerType === "tgi"
-            ? `763104351884.dkr.ecr.us-east-1.amazonaws.com/${ecrRepository}:2.4.0-tgi2.3.1-gpu-py311-cu124-ubuntu22.04`
-            : `763104351884.dkr.ecr.us-east-1.amazonaws.com/${ecrRepository}:0.31.0-lmi13.0.0-cu124`;
-
-        const sagemakerRole = new SageMakerIamRoleConstruct(this, "SageMakerRole", {
-            ecrRepository
-        });
-
-        const gpuMapping: { [key: number]: string[] } = {
-            4: ["ml.g5.12xlarge", "ml.g6e.24xlarge", "ml.g6e.12xlarge", "ml.g6.12xlarge", "ml.g6.24xlarge", "ml.g4dn.12xlarge", "ml.g4ad.16xlarge"],
-            8: ["ml.p5.48xlarge", "ml.p5e.48xlarge", "ml.p5en.48xlarge", "ml.p4d.24xlarge", "ml.p4de.24xlarge", "ml.g6e.48xlarge", "ml.g6.48xlarge"],
-        };
-
         const instanceType = props.instanceType || "ml.g5.2xlarge";
-
-        let numberOfGpu = 1; // Default
-        for (const [gpuCount, instanceList] of Object.entries(gpuMapping)) {
-            if (instanceList.includes(instanceType)) {
-                numberOfGpu = parseInt(gpuCount);
-                break;
-            }
-        }
 
         const hfModelId = `deepseek-ai/DeepSeek-R1-Distill-${props.modelName}`;
 
         const model = new sagemaker.CfnModel(this, "DeepSeekR1Model", {
-            executionRoleArn: sagemakerRole.roleArn,
+            executionRoleArn: props.sagemakerRole.roleArn,
             primaryContainer: {
-                image: inferenceImageUri,
+                image: props.inferenceImageUri,
                 environment: {
                     "HF_MODEL_ID": hfModelId,
                     "OPTION_MAX_MODEL_LEN": "10000",
@@ -59,12 +37,11 @@ export class DeepSeekR1SageMakerConstruct extends Construct {
                     "OPTION_DTYPE": "fp16",
                     "MAX_CONCURRENT_REQUESTS": "10",
                     "PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True",
-                    "SM_NUM_GPUS": JSON.stringify(numberOfGpu)
+                    "SM_NUM_GPUS": JSON.stringify(props.numberOfGpu)
                 }
             },
             modelName: props.modelName
         });
-        model.node.addDependency(sagemakerRole);
 
         const endpointConfig = new sagemaker.CfnEndpointConfig(this, "DeepSeekR1EndpointConfig", {
             productionVariants: [{
--- lib/deepseek-r1-sagemaker-stack.ts
+++ lib/deepseek-r1-sagemaker-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { DeepSeekR1SageMakerConstruct } from './constructs/deepseek-r1-sagemaker-construct';
+import {SageMakerIamRoleConstruct} from "./constructs/deepseek-r1-sagemaker-role-construct";
 
 export class DeepSeekR1SageMakerStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -9,11 +10,40 @@ export class DeepSeekR1SageMakerStack extends cdk.Stack {
     const instanceType = this.node.tryGetContext("instanceType") || "ml.g5.2xlarge";
     const modelName = this.node.tryGetContext("modelName") || "Qwen-7B";
 
+    const ecrRepository = containerType === "tgi"
+        ? "huggingface-pytorch-tgi-inference"
+        : "djl-inference";
+
+    const inferenceImageUri = containerType === "tgi"
+        ? `763104351884.dkr.ecr.us-east-1.amazonaws.com/${ecrRepository}:2.4.0-tgi2.3.1-gpu-py311-cu124-ubuntu22.04`
+        : `763104351884.dkr.ecr.us-east-1.amazonaws.com/${ecrRepository}:0.31.0-lmi13.0.0-cu124`;
+
+    const gpuMapping: { [key: number]: string[] } = {
+      4: ["ml.g5.12xlarge", "ml.g6e.24xlarge", "ml.g6e.12xlarge", "ml.g6.12xlarge", "ml.g6.24xlarge", "ml.g4dn.12xlarge", "ml.g4ad.16xlarge"],
+      8: ["ml.p5.48xlarge", "ml.p5e.48xlarge", "ml.p5en.48xlarge", "ml.p4d.24xlarge", "ml.p4de.24xlarge", "ml.g6e.48xlarge", "ml.g6.48xlarge"],
+    };
+
+    let numberOfGpu = 1;
+    for (const [gpuCount, instanceList] of Object.entries(gpuMapping)) {
+      if (instanceList.includes(instanceType)) {
+        numberOfGpu = parseInt(gpuCount);
+        break;
+      }
+    }
+
+    const sagemakerRole = new SageMakerIamRoleConstruct(this, "SageMakerRole", {
+      ecrRepository
+    });
+
     const sagemakerConstruct = new DeepSeekR1SageMakerConstruct(this, "DeepSeekR1SageMakerConstruct", {
       containerType,
       instanceType,
-      modelName
+      modelName,
+      sagemakerRole,
+      inferenceImageUri,
+      numberOfGpu
     });
+    sagemakerConstruct.node.addDependency(sagemakerRole);
 
     new cdk.CfnOutput(this, "SageMakerEndpointName", {
       value: sagemakerConstruct.sagemakerEndpointName,
